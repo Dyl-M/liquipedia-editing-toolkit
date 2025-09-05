38e184599b65ba525707fe0ec0fdc390f489177a
dyl_m <dyl_m.dev@proton.me>
2025-09-05 16:25:54 +0200

    chore: some small adjustments in the sandbox.

--- src/_sandbox.py
+++ src/_sandbox.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
+# Built-in and third-party imports
 import json
-import requests
+
+# Local imports
+import liquipedia_tools as lp_t
 import startgg_tools as sgg_t
 
 """File Information
@@ -11,15 +14,12 @@ To test things / backup functions.
 """
 
 if __name__ == "__main__":
-    from startgg_tools import get_event_top_teams
-    from liquipedia_tools import generate_team_cards_tabs_from_json
-
     event_slug = "tournament/3v3-sam-champions-road-2025/event/3v3-bracket"
-    top_teams = get_event_top_teams(event_slug, top_n=32)
+    top_teams = sgg_t.get_event_top_teams(event_slug, top_n=32)
 
     with open(f'../data/{event_slug.split('/')[1]}.json', 'w', encoding='utf-8') as json_file:
         json.dump(top_teams, json_file, indent=4, ensure_ascii=False)
 
-    print(generate_team_cards_tabs_from_json("../data/3v3-sam-champions-road-2025.json",
-                                             [12, 32],
-                                             remove_empty_optional=True))
+    print(lp_t.generate_team_cards_tabs_from_json("../data/3v3-sam-champions-road-2025.json",
+                                                  [12, 32],
+                                                  remove_empty_optional=True))
